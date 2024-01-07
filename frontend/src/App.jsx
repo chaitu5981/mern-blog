87906efb170ea63f9344edbd68ce3a44d45115c9
Chaitanya <chaitu.raju@gmail.com>
2024-01-07 21:29:25 +0530

    Routes and Pages created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 const App = () => {
-  return <div className="text-3xl text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
